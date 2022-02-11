03b84165e3841e17034110965032fb15e0c19ae7
Frederick Nyawaya <o.frederickn@gmail.com>
2022-02-11 16:07:17 +0300

    upped lib with renamed phone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_11_124443) do
+ActiveRecord::Schema.define(version: 2022_02_11_130708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -301,7 +301,7 @@ ActiveRecord::Schema.define(version: 2022_02_11_124443) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "phone"
+    t.string "payment_instrument_number"
     t.string "name"
   end
 
